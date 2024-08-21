a79143aca387a8e094cd3886777711806d757216
Mohamed Alsamman <sammantic4@gmail.com>
2024-08-21 21:28:37 +0200

    Check if the token is setted in the header or not

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -3,16 +3,17 @@ const jwt = require("jsonwebtoken");
 // Verify token
 function verifyToken(req, res, next) {
     const token = req.headers.token;
+    
     if (token) {
         try {
             const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
             req.user = decoded
             next()
         } catch {
-            res.status(401).json({ message: "Invalid token" });
+            return res.status(401).json({ message: "Invalid token" });
         }
     } else {
-        res.status(401).json({ message: "No token provided" });
+        return res.status(401).json({ message: "No token provided" });
     }
 }
 
@@ -28,13 +29,17 @@ function verifyTokenAndAuthorization(req, res, next) {
 }
 
 function verifyTokenAndAuthorizationAdmin(req, res, next) {
-    if (req.user.isAdmin) {
-        next()
-    } else {
-        return res.status(403).json({ message: "You are not allowed, only admin" });
-    }
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin) {
+            next()
+        } else {
+            return res.status(403).json({ message: "You are not allowed, only admin" });
+        }
+    });
 }
+
 module.exports = {
     verifyToken,
-    verifyTokenAndAuthorization
+    verifyTokenAndAuthorization,
+    verifyTokenAndAuthorizationAdmin
 }
